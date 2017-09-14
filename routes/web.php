2d6c1f39b80ee5a0339072686ba48f68ef9f4699
vi.al.kravchenko <kravchenkovit5@gmail.com>
2017-09-14 16:30:30 +0300

    second test commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//маршруты
 Route::get('/', 'PageController@showHome' );
 Route::get('/requests', 'PageController@showRequests' );
 
